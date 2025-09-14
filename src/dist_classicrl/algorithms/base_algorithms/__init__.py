5a9b0247d969c555002dd78be7929bb387a2c45c
Javier Moralejo Piñas <j.moralejo.pinas@gmail.com>
2025-09-14 20:15:30 +0200

    test

--- src/dist_classicrl/algorithms/base_algorithms/__init__.py
+++ src/dist_classicrl/algorithms/base_algorithms/__init__.py
@@ -2,5 +2,5 @@
 Base Algorithm Implementations.
 
 This package contains the core implementations of fundamental reinforcement learning algorithms that
-serve as building blocks for more complex distributed methods.
+serve as building blocks for more complex distributed methods.test
 """
